c00756e40ce0be6d0efa2531e2b3994e350ef115
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-06-14 14:17:53 +0300

    PORT

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import 'dotenv/config';
+
+const { PORT = 3000 } = process.env;
 
 export const setupServer = () => {
   const app = express();
 
-  const logger = pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  });
+  // const logger = pino({
+  //   transport: {
+  //     target: 'pino-pretty',
+  //   },
+  // });
 
-  app.use(logger);
-  app.use(cors());
+  // app.use(logger);
+  app.use('*', cors());
 
   //   app.get('/api/contacts', (req, res) => {
   //     res.json(contacts);
