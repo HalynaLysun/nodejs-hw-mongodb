395d3c93141dd3ada968079f4f24095ba0e7001e
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-07-12 11:01:35 +0300

    token

--- src/constans/auth.js
+++ src/constans/auth.js
@@ -1,8 +1,8 @@
 export const emailReg =
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-export const ACCESS_TOKEN_LIFETIME = 10 * 24 * 3600 * 1000;
-// 45 * 60 * 1000
+export const ACCESS_TOKEN_LIFETIME = 45 * 60 * 1000;
+
 export const REFRESH_TOKEN_LIFETIME = 30 * 24 * 3600 * 1000;
 
 export const SMTP = {
