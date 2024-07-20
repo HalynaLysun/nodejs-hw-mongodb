9d740ac421f2ce8e2720450a39634fd62ace4dfa
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-07-20 08:55:41 +0300

    cor

--- src/constans/contacts.js
+++ src/constans/contacts.js
@@ -1,4 +1,4 @@
 import path from 'node:path';
 export const typeList = ['work', 'home', 'personal'];
 
-export const SWAGGER_PATH = path.resolve(process.cwd(), 'docs', 'swagger.json');
+export const SWAGGER_PATH = path.resolve('docs', 'swagger.json');
