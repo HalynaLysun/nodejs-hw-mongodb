b6acf9d3e2b1debc103f317ff5254bd88387fbb7
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-07-22 19:44:51 +0300

    cor

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -6,8 +6,9 @@ import { SWAGGER_PATH } from '../constans/contacts.js';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
     console.log(SWAGGER_PATH, swaggerDoc);
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    console.log('1');
     return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
   } catch (err) {
     return (req, res, next) =>
